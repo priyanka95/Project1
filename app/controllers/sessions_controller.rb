a89c9ac8fcea75efe2f536fae0e3e29e81c0587e
unknown <admin1@VIT-WINL05D-02.wdc.vidyalankarlive.com>
2014-02-02 15:03:16 +0530

    Finish user posts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
   	user=User.find_by_email(params[:email])
   	if user && user.authenticate(params[:password])
-  		session[:user_id] = @user.id
+  		session[:user_id] = user.id
   		redirect_to user,notice: "Logged in!"
   	else
   		flash[:error]= "Wrong email or password"
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ def create
 	@user = User.new(user_params)
 
 	if @user.save
-		session[:user_id] =user.id
+		session[:user_id] = @user.id
 		redirect_to @user
 	else
 		render 'new'
@@ -17,6 +17,7 @@ end
 
 def show
 	@user =User.find(params[:id])
+	@post =Post.new
 end
 
 private
